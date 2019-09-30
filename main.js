c1abc5a86092e09a844990e1563eb9cd5a26ba9a
Michael Burnes <me@mtburnes.dev>
2019-09-30 11:45:57 -0500

    Attempted Finish.

--- main.js
+++ main.js
@@ -1,51 +1,38 @@
-const add = (x, y) => { return x + y }
+// This section of code is used from the forked repo. I want to use my own function.
+//<begin commented code>
 
-const validate = async (event) => {
-  console.log(`triggered validate on ${event.target.id}`)
-  const isValid = event.target.checkValidity()
-  if (isValid) {
-    event.target.nextElementSibling.innerHTML = ''
-  } else {
-    event.target.nextElementSibling.innerHTML = 'Invalid input'
-    event.target.focus()
-  }
-}
+// const updateWithAdd = async (event) => {
+//   document.querySelector('#result').innerHTML = ''
+//   if (document.querySelector('#itemsPerBag').checkValidity() && document.querySelector('#bags').checkValidity()) {
+//     const i = parseInt(document.querySelector('#itemsPerBag').value)
+//     const j = parseInt(document.querySelector('#bags').value)
+//     const multAns = (i * j)
+//     const ans =  `There are ${multAns} items total.`
+//     document.querySelector('#result').innerHTML = ans
+//   }
+// }
 
-const updateWithAdd = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
-    const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
-    document.querySelector('#result').innerHTML = ans
-  }
-}
-
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
 
 // delegate to dynamic elements (e.g. when testing)
 // focusout is like blur, but it bubbles up
 
-document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
-    validate(event)
-  }
-})
+// document.addEventListener('click', event => {
+  //   if (event.target && event.target.id === 'calculate') { updateWithAdd(event) }
+// })
+//</end commented code>
+  
+function updateResult(value){
+  document.querySelector('#result').innerHTML = `The total number of items is ${value}`;
+}
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
-})
+function calculate(){
+  const items = parseInt(document.getElementById("itemsPerBag"));
+  const bagCount = parseInt(document.getElementById("bags"));
+  const calculatedTotal = items * bagCount;
+  updateResult(calculatedTotal);
+}
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
+function testCalculate(x, y){
+  const result = x * y;
+  console.log(`The result should be ${result}`);
+}
